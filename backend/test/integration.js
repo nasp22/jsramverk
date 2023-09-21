cf1c069967faa1f60dbfd6b792be93fe84edecdb
Nadja Spångberg <nasp22@student.bth.se>
2023-09-21 11:30:10 +0200

    added test for POST /tickets

--- backend/test/integration.js
+++ backend/test/integration.js
@@ -1,19 +1,19 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../app.js');
-// const tickets = require('../models/tickets.js');
+const tickets = require('../models/tickets.js');
 const mocha = require('mocha');
 
 chai.should();
 chai.use(chaiHttp);
 
-// var currentdate = new Date();
-// var datetime = currentdate.getDate() + "/"
-//                 + (currentdate.getMonth()+1)  + "/"
-//                 + currentdate.getFullYear() + " @ "
-//                 + currentdate.getHours() + ":"
-//                 + currentdate.getMinutes() + ":"
-//                 + currentdate.getSeconds();
+var currentdate = new Date();
+var datetime = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/"
+                + currentdate.getFullYear() + " @ "
+                + currentdate.getHours() + ":"
+                + currentdate.getMinutes() + ":"
+                + currentdate.getSeconds();
 
 mocha.describe('Test integration to server and API', () => {
     mocha.describe('START /app/httpServer', () => {
@@ -74,29 +74,29 @@ mocha.describe('Test integration to server and API', () => {
         });
     });
 
-    // mocha.describe('POST /routes/tickets.js', () => {
-    //     mocha.it('200 HAPPY PATH', (done) => {
-    //         process.env.NODE_ENV = "test";
-    //         let req = {
-    //             body: {
-    //                 code: "Test",
-    //                 trainnumber: "run",
-    //                 traindate: datetime
-    //             }
-    //         };
+    mocha.describe('POST /routes/tickets.js', () => {
+        mocha.it('200 HAPPY PATH', (done) => {
+            process.env.NODE_ENV = "test";
+            let req = {
+                body: {
+                    code: "Test",
+                    trainnumber: "run",
+                    traindate: datetime
+                }
+            };
 
-    //         let res = tickets.createTicket(req);
+            tickets.createTicket(req);
 
-    //         chai.request(server)
-    //             .get("/tickets")
-    //             .end((err, res) => {
-    //                 res.should.have.status(200);
-    //                 res.body.should.be.an("object");
-    //                 res.body.data.should.be.an("array");
-    //                 res.body.data.length.should.be.above(0);
+            chai.request(server)
+                .get("/tickets")
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.an("object");
+                    res.body.data.should.be.an("array");
+                    res.body.data.length.should.be.above(0);
 
-    //                 done();
-    //             });
-    //     });
-    // });
+                    done();
+                });
+        });
+    });
 });
