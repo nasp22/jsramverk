ca31f4b482b7e890135a45d2169679fee43c3c46
Bruffe <gabbe.rahm@gmail.com>
2023-09-26 00:10:08 +0200

    Installed MongoDB and updated related code

--- backend/models/tickets.js
+++ backend/models/tickets.js
@@ -1,37 +1,63 @@
 const database = require('../db/database.js');
+const collectionName = "tickets";
+let dbName = "trains";
+
+if (process.env.NODE_ENV === 'test') {
+    dbName = "trains-test";
+}
 
 const tickets = {
     getTickets: async function getTickets(req, res) {
-        var db = await database.openDb();
+        // Access a MongoClient object
+        const client = await database.accessDb();
 
-        var allTickets = await db.all(`SELECT *, ROWID as id FROM tickets ORDER BY ROWID DESC`);
+        // Connect to the database
+        await client.connect();
 
-        await db.close();
+        // Get the database object
+        const db = client.db(dbName);
 
+        // Get the collection object
+        const collection = db.collection(collectionName);
+
+        // Get all documents (tickets) in the collection (trains)
+        let allTickets = await collection.find().toArray();
+
+        // Close the database connection
+        await client.close();
+
+        // Print all documents to the console
+        console.log(allTickets);
         return res.json({
             data: allTickets
         });
     },
 
     createTicket: async function createTicket(req, res) {
-        var db = await database.openDb();
+        // Access a MongoClient object
+        const client = await database.accessDb();
 
-        const result = await db.run(
-            'INSERT INTO tickets (code, trainnumber, traindate) VALUES (?, ?, ?)',
-            req.body.code,
-            req.body.trainnumber,
-            req.body.traindate,
-        );
+        // Connect to the database
+        await client.connect();
 
-        await db.close();
+        // Get the database object
+        const db = client.db(dbName);
 
+        // Get the collection object
+        const collection = db.collection(collectionName);
+
+        // Create new document (ticket) in the collection (trains)
+        let newTicket = req.body;
+
+        await collection.insertOne(newTicket);
+
+        // Close the database connection
+        await client.close();
+
+        // Print all documents to the console
+        console.log(newTicket);
         return res.json({
-            data: {
-                id: result.lastID,
-                code: req.body.code,
-                trainnumber: req.body.trainnumber,
-                traindate: req.body.traindate,
-            }
+            data: newTicket
         });
     }
 };
