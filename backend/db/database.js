8bad162520536d2aa1137dc9a162c76e418ec2e1
Bruffe <gabbe.rahm@gmail.com>
2023-09-27 19:57:40 +0200

    Removed MongoDB hardcoding

--- backend/db/database.js
+++ backend/db/database.js
@@ -5,14 +5,11 @@ const database = {
         let dsn = `mongodb+srv://Bruffe:${process.env.MONGODB_PASSWORD}@jsramverk \
         .cpm6rtk.mongodb.net/?retryWrites=true&w=majority`;
 
-        // console.log(process.env.MONGODB_DB_NAME);
-        // console.log(env.MONGODB_DB_NAME);
-        // console.log(MONGODB_DB_NAME);
+        console.log(process.env.MONGODB_DB_NAME);
         if (process.env.MONGODB_DB_NAME) {
             console.log("GitHub Actions env variable accessed");
             dsn = "mongodb://localhost:27017";
         }
-        dsn = "mongodb://localhost:27017"; // test
 
         const client = new MongoClient(dsn);
 
