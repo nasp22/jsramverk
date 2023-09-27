f7be5d42fdbc4b00b9d34c3ed8dda7bb715e8743
Nadja Spångberg <117263836+nasp22@users.noreply.github.com>
2023-09-27 19:33:01 +0200

    Update integration.js

--- backend/test/integration.js
+++ backend/test/integration.js
@@ -73,29 +73,4 @@ mocha.describe('Test integration to server and API', () => {
         });
     });
 
-    mocha.describe('POST /routes/tickets.js', () => {
-        mocha.it('200 HAPPY PATH', (done) => {
-            process.env.NODE_ENV = "test";
-            let req = {
-                body: {
-                    code: "Test",
-                    trainnumber: "run",
-                    traindate: datetime
-                }
-            };
-
-            tickets.createTicket(req);
-
-            chai.request(server)
-                .get("/tickets")
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    // res.body.should.be.an("object");
-                    // res.body.data.should.be.an("array");
-                    // res.body.data.length.should.be.above(0);
-
-                    done();
-                });
-        });
-    });
 });
