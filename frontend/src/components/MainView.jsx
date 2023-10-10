eb5efeaec313323f7e94985c3256dfe767670e28
Nadja Spångberg <nasp22@student.bth.se>
2023-10-10 10:56:57 +0200

    Show only delayed trains

--- frontend/src/components/MainView.jsx
+++ frontend/src/components/MainView.jsx
@@ -1,23 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DelayTableView from './DelayTableView';
 import TicketView from './TicketView';
 import Map from './Map';
+import config from '../config.js';
 
+const apiUrl = config;
 const MainView = () => {
   const [selectedTrain, setSelectedTrain] = useState(null);
+  const [delayedData, setDelayedData] = useState([]);
+
+  useEffect(() => {
+    fetchDelayedData();
+  }, []);
+
+  const fetchDelayedData = () => {
+    fetch(`${apiUrl}/delayed`)
+      .then((response) => response.json())
+      .then((data) => setDelayedData(data.data))
+      .catch((error) => console.error('Error fetching delayed data:', error));
+  };
 
   const handleTrainClick = (train) => {
     setSelectedTrain(train);
   };
 
-  return (<>
+  return (
+    <>
       {selectedTrain ? (
         <TicketView selectedTrain={selectedTrain} onBackClick={() => setSelectedTrain(null)} />
       ) : (
-        <DelayTableView onTrainClick={handleTrainClick} />
+        <DelayTableView onTrainClick={handleTrainClick} delayedData={delayedData} />
       )}
-      <Map/>
-      </>
+      <Map delayedData={delayedData} /> {/* Skicka delayedData till Map */}
+    </>
   );
 };
 
--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,13 +1,13 @@
-import io from 'socket.io-client'
+import io from 'socket.io-client';
 import React, { useEffect, useRef } from 'react';
 import * as L from 'leaflet';
-import markerImg from '../icon/location.png'
+import markerImg from '../icon/location.png';
 import config from '../config.js';
 
 const apiUrl = config;
-console.log(`utskrift apiURL i map.jsx = ${apiUrl}`)
+console.log(`utskrift apiURL i map.jsx = ${apiUrl}`);
 
-const Map = () => {
+const Map = ({ delayedData }) => {
   const mapRef = useRef(null);
   const markers = useRef({});
 
@@ -17,30 +17,32 @@ const Map = () => {
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
     var myMarker = L.icon({
       iconUrl: markerImg,
-      iconSize:     [38, 38],
-  });
+      iconSize: [38, 38],
+    });
 
     const socket = io(`${apiUrl}`);
 
-    socket.on("message", (data) => {
-      if (markers.current.hasOwnProperty(data.trainnumber)) {
-        const marker = markers.current[data.trainnumber];
-        marker.setLatLng(data.position);
-      } else {
-        const marker = L.marker(data.position, {icon: myMarker}).bindPopup(data.trainnumber).addTo(map);
-        markers.current[data.trainnumber] = marker;
+    socket.on('message', (data) => {
+      if (delayedData.find((item) => item.OperationalTrainNumber === data.trainnumber)) {
+        if (markers.current.hasOwnProperty(data.trainnumber)) {
+          const marker = markers.current[data.trainnumber];
+          marker.setLatLng(data.position);
+        } else {
+          const marker = L.marker(data.position, { icon: myMarker }).bindPopup(data.trainnumber).addTo(map);
+          markers.current[data.trainnumber] = marker;
+        }
       }
     });
 
     return () => {
       map.remove();
     };
-  }, []);
+  }, [delayedData]);
 
   return <div id="map" className="map"></div>;
 };
