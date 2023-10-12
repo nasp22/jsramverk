2ee9c1d19a25e5f6a4a77151fcb3f85ddfe80e8e
Bruffe <gabbe.rahm@gmail.com>
2023-10-13 01:50:44 +0200

    Updated integration tests so they now test GraphQL routes

--- backend/test/integration.js
+++ backend/test/integration.js
@@ -1,7 +1,6 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../app.js');
-const tickets = require('../models/tickets.js');
 const mocha = require('mocha');
 
 chai.should();
@@ -30,68 +29,133 @@ mocha.describe('Test integration to server and API', () => {
         });
     });
 
-    mocha.describe('GET /routes/codes.js', () => {
-        mocha.it('200 HAPPY PATH', (done) => {
+    mocha.describe('Fetch codes with GraphQL', () => {
+        mocha.it('should retrieve a list of codes', (done) => {
             chai.request(server)
-                .get("/codes")
+                .post("/graphql")
+                .send({ query: `
+                { 
+                    codes { 
+                        Code, 
+                        Level1Description, 
+                        Level2Description, 
+                        Level3Description 
+                    } 
+                }`
+                })
                 .end((err, res) => {
                     res.should.have.status(200);
-                    res.body.should.be.an("object");
-                    res.body.data.should.be.an("array");
-                    res.body.data.length.should.be.above(0);
+                    res.body.data.should.be.an("object");
+                    res.body.data.codes.should.be.an("array");
+                    res.body.data.codes.length.should.be.above(0);
                     done();
                 });
         });
     });
 
-    mocha.describe('GET /routes/delayed.js', () => {
-        mocha.it('200 HAPPY PATH', (done) => {
+    mocha.describe('Fetch delays with GraphQL', () => {
+        mocha.it('should retrieve a list of delayed trains', (done) => {
             chai.request(server)
-                .get("/delayed")
+                .post("/graphql")
+                .send({ query: `
+                {
+                    delays { 
+                        ActivityId, 
+                        ActivityType, 
+                        AdvertisedTimeAtLocation, 
+                        AdvertisedTrainIdent
+                        Canceled
+                        EstimatedTimeAtLocation
+                        FromLocation {
+                          LocationName
+                          Priority
+                          Order
+                        }
+                        LocationSignature
+                        OperationalTrainNumber
+                        ToLocation {
+                          LocationName
+                          Priority
+                          Order
+                        }
+                        TrainOwner
+                      }
+                }`
+                })
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.an("object");
-                    res.body.data.should.be.an("array");
-                    res.body.data.length.should.be.above(0);
+                    res.body.data.delays.should.be.an("array");
+                    res.body.data.delays.length.should.be.above(0);
 
                     done();
                 });
         });
     });
 
-    mocha.describe('POST /routes/tickets.js', () => {
-        mocha.it('200 HAPPY PATH', (done) => {
-            let req = {
-                body: {
+    mocha.describe('Create new ticket with GraphQL', () => {
+        mocha.it('should retrieve info about new ticket', (done) => {
+            const requestBody = {
+                query: `
+                    mutation CreateNewTicket(
+                        $code: String!, 
+                        $trainnumber: String!, 
+                        $traindate: String!
+                        ) 
+                        {
+                        createTicket(
+                            code: $code, 
+                            trainnumber: $trainnumber, 
+                            traindate: $traindate) 
+                        {
+                            _id
+                            code
+                            trainnumber
+                            traindate
+                        }
+                    }`,
+                variables: {
                     code: "Test",
                     trainnumber: "run",
                     traindate: datetime
                 }
             };
 
-            tickets.createTicket(req);
-
             chai.request(server)
-                .get("/tickets")
+                .post("/graphql")
+                .send(requestBody)
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.an("object");
-                    res.body.data.should.be.an("array");
-
+                    res.body.data.createTicket.should.be.an("object");
+                    res.body.data.createTicket.should.have.property("_id");
+                    res.body.data.createTicket.should.have.property("code");
+                    res.body.data.createTicket.should.have.property("trainnumber");
+                    res.body.data.createTicket.should.have.property("traindate");
                     done();
                 });
         });
     });
 
-    mocha.describe('GET /routes/tickets.js', () => {
-        mocha.it('200 HAPPY PATH', (done) => {
+    mocha.describe('Fetch tickets with GraphQL', () => {
+        mocha.it('should retrieve a list of all tickets', (done) => {
             chai.request(server)
-                .get("/tickets")
+                .get("/graphql")
+                .send({ query: `
+                {
+                    tickets {
+                        _id,
+                        code,
+                        trainnumber,
+                        traindate
+                    }
+                }`
+                })
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.an("object");
-                    res.body.data.should.be.an("array");
-                    res.body.data.length.should.be.above(0);
+                    res.body.data.tickets.should.be.an("array");
+                    res.body.data.tickets.length.should.be.above(0);
 
                     done();
                 });
