511e3bf8b4646f77f47f6faf5a4e737977116655
Nadja Spångberg <nasp22@student.bth.se>
2023-10-24 14:41:33 +0200

    changed DelayView so the fetch only is done one time in mainView

--- frontend/src/components/DelayTableView.jsx
+++ frontend/src/components/DelayTableView.jsx
@@ -4,50 +4,7 @@ import config from '../config.js';
 const apiUrl = config;
 // console.log(`utskrift apiURL i delaytableview.jsx = ${apiUrl}`)
 
-const DelayTableView = ({ onTrainClick }) => {
-  const [delayedData, setDelayedData] = useState([]);
-
-  useEffect(() => {
-    fetchDelayedData();
-  }, []);
-
-  const fetchDelayedData = () => {
-    // fetch(`${apiUrl}/delayed`)
-    fetch(`${apiUrl}/graphql`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      body: JSON.stringify({ query: `
-      { 
-        delays { 
-          ActivityId, 
-          ActivityType, 
-          AdvertisedTimeAtLocation, 
-          AdvertisedTrainIdent
-          Canceled
-          EstimatedTimeAtLocation
-          FromLocation {
-            LocationName
-            Priority
-            Order
-          }
-          LocationSignature
-          OperationalTrainNumber
-          ToLocation {
-            LocationName
-            Priority
-            Order
-          }
-          TrainOwner
-        }
-      }` })
-    })
-      .then((response) => response.json())
-      .then((data) => setDelayedData(data.data.delays))
-      .catch((error) => console.error('Error fetching delayed data:', error));
-  };
+const DelayTableView = ({ onTrainClick, delayedData }) => {
 
   const outputDelay = (item) => {
     let advertised = new Date(item.AdvertisedTimeAtLocation);
